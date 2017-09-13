e07fffdd89a821c53db2e2b21e5cc2827ac49d31
Ross Harper <ross.harper@bbc.co.uk>
2017-09-13 16:54:47 +0100

    Extract parse

--- src/main/kotlin/net/rossharper/awshelloworld/Main.kt
+++ src/main/kotlin/net/rossharper/awshelloworld/Main.kt
@@ -14,9 +14,7 @@ data class Error(val error: String) : HandlerOutput()
 
 class Main {
     fun handler(input: InputStream, output: OutputStream): Unit {
-        val moshi = Moshi.Builder().build()
-        val inputAdapter : JsonAdapter<HandlerInput> = moshi.adapter(HandlerInput::class.java)
-        val handlerInput = inputAdapter.fromJson(Okio.buffer(Okio.source(input)))
+        val handlerInput = input.parse()
 
         if(handlerInput == null) {
             output with Error("Bad Request")
@@ -24,15 +22,20 @@ class Main {
             output with Greeting("Hello, ${handlerInput.name}. Welcome to my Kotlin AWS SuperLambBanana")
         }
     }
+}
 
-    inline infix fun <reified T : HandlerOutput> OutputStream.with(response: T) {
-        val bufferedSink = Okio.buffer(Okio.sink(this))
-        Moshi.Builder()
-                .build()
-                .adapter(T::class.java)
-                .toJson(bufferedSink, response)
-        bufferedSink.emit()
-    }
+private fun InputStream.parse(): HandlerInput? {
+    val moshi = Moshi.Builder().build()
+    val inputAdapter : JsonAdapter<HandlerInput> = moshi.adapter(HandlerInput::class.java)
+    return inputAdapter.fromJson(Okio.buffer(Okio.source(this)))
+}
 
+private inline infix fun <reified T : HandlerOutput> OutputStream.with(response: T) {
+    val bufferedSink = Okio.buffer(Okio.sink(this))
+    Moshi.Builder()
+            .build()
+            .adapter(T::class.java)
+            .toJson(bufferedSink, response)
+    bufferedSink.emit()
 }
 
