44ac2d285235600bb46205a1da7d5aabbf127b23
Ross Harper <ross.harper@bbc.co.uk>
2017-09-14 17:52:56 +0100

    Putting together SAM Cloudformation deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+aws cloudformation deploy \
+--template-file sam_template.yaml \
+--stack-name awshelloworld \
+--region eu-west-1 \
+--capabilities CAPABILITY_IAM
---
+++ uploadS3.sh
@@ -0,0 +1,2 @@
+aws s3 mb s3://awshelloworld
+aws s3 cp build/libs/aws-lambda-kotlin-helloworld-1.0-SNAPSHOT-all.jar s3://awshelloworld/aws-lambda-kotlin-helloworld-1.0-SNAPSHOT-all.jar --region eu-west-1
