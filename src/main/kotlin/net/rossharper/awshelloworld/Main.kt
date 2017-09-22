5b507b762618c6093bc824e879f8ebf34571101e
Ross Harper <ross.harper@bbc.co.uk>
2017-09-22 14:54:39 +0100

    working sam template

--- src/main/kotlin/net/rossharper/awshelloworld/Main.kt
+++ src/main/kotlin/net/rossharper/awshelloworld/Main.kt
@@ -6,11 +6,15 @@ import okio.Okio
 import java.io.InputStream
 import java.io.OutputStream
 
+data class Request(val body: String)
+
 data class HandlerInput(val name: String)
 
 sealed class HandlerOutput
 data class Greeting(val greeting: String) : HandlerOutput()
 data class Error(val error: String) : HandlerOutput()
+
+data class Response(val statusCode: Int, val body: String)
 
 class Main {
     fun handler(input: InputStream, output: OutputStream): Unit {
@@ -26,15 +30,28 @@ class Main {
 
 private fun InputStream.parse(): HandlerInput? {
     val moshi = Moshi.Builder().build()
-    val inputAdapter : JsonAdapter<HandlerInput> = moshi.adapter(HandlerInput::class.java)
-    return inputAdapter.fromJson(Okio.buffer(Okio.source(this)))
+    val requestAdapter : JsonAdapter<Request> = moshi.adapter(Request::class.java)
+    val body = requestAdapter.fromJson(Okio.buffer(Okio.source(this)))?.body
+    body?.let {
+        val inputAdapter : JsonAdapter<HandlerInput> = moshi.adapter(HandlerInput::class.java)
+        return inputAdapter.fromJson(it)
+    }
+    return null
 }
 
-private inline infix fun <reified T : HandlerOutput> OutputStream.with(response: T) {
+private inline infix fun <reified T : HandlerOutput> OutputStream.with(output: T) {
+    val body = Moshi.Builder().build().adapter(T::class.java).toJson(output)
+    when(output) {
+        is Greeting -> this.with(Response(200, body))
+        is Error -> this.with(Response(500, body))
+    }
+}
+
+private infix fun OutputStream.with(response : Response) {
     val bufferedSink = Okio.buffer(Okio.sink(this))
     Moshi.Builder()
             .build()
-            .adapter(T::class.java)
+            .adapter(Response::class.java)
             .toJson(bufferedSink, response)
     bufferedSink.emit()
 }
--- src/test/kotlin/HandlerShould.kt
+++ src/test/kotlin/HandlerShould.kt
@@ -1,37 +1,18 @@
-import com.squareup.moshi.Moshi
-import okio.Okio
-import okio.Sink
+package net.rossharper.awshelloworld
+
 import org.junit.Test
 import java.io.ByteArrayOutputStream
-import java.io.InputStream
-import java.io.InputStreamReader
 
 class HandlerShould {
 
-    data class Input(val input: String)
-    data class Output(val output: String)
-
     @Test
     fun not_a_test_but_a_spike_using_moshi_with_okio() {
-        val input = "{\"input\":\"input\"}"
+        val input = "{\"resource\":\"/test\",\"path\":\"/test\",\"httpMethod\":\"POST\",\"headers\":null,\"queryStringParameters\":null,\"pathParameters\":null,\"stageVariables\":null,\"requestContext\":{\"path\":\"/test\",\"accountId\":\"373806372146\",\"resourceId\":\"4hcbe8\",\"stage\":\"test-invoke-stage\",\"requestId\":\"test-invoke-request\",\"identity\":{\"cognitoIdentityPoolId\":null,\"accountId\":\"373806372146\",\"cognitoIdentityId\":null,\"caller\":\"AIDAIVLN52O7EH3NKQTVG\",\"apiKey\":\"test-invoke-api-key\",\"sourceIp\":\"test-invoke-source-ip\",\"accessKey\":\"ASIAJVOYDV6SFJL7F5KQ\",\"cognitoAuthenticationType\":null,\"cognitoAuthenticationProvider\":null,\"userArn\":\"arn:aws:iam::373806372146:user/Administrator\",\"userAgent\":\"Apache-HttpClient/4.5.x (Java/1.8.0_131)\",\"user\":\"AIDAIVLN52O7EH3NKQTVG\"},\"resourcePath\":\"/test\",\"httpMethod\":\"POST\",\"apiId\":\"gxkb6e8i28\"},\"body\":\"{\\\"name\\\":\\\"Ross\\\"}\",\"isBase64Encoded\":false}"
         val inputStream = input.byteInputStream()
-        val inputObj = Moshi.Builder().build().adapter<Input>(Input::class.java).fromJson(Okio.buffer(Okio.source(inputStream)))
-
-        println(inputObj)
-
-        val output = Output("output")
 
         val outputStream = ByteArrayOutputStream()
 
-        outputStream.write("hello".toByteArray())
-
-        println(outputStream.toString())
-
-        outputStream.reset()
-
-        val sink = Okio.buffer(Okio.sink(outputStream))
-        Moshi.Builder().build().adapter<Output>(Output::class.java).toJson(sink, output)
-        sink.emit()
+        Main().handler(inputStream, outputStream)
 
         val outputStr = outputStream.toString()
 
